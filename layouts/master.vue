9e241e6ca0090ce22e38b8b3a9e9acceb74d2532
Paolo Cantoni <paul@paolocantoni.com>
2022-05-27 17:11:03 +0200

    first commit

---
+++ layouts/master.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+   Layout master
+    <slot />
+  </div>
+</template>
+
---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <!--  <v-app>-->
+  <!--    <v-navigation-drawer color="grey-darken-2" permanent></v-navigation-drawer>-->
+  <!--    <v-app-bar color="grey-lighten-2"></v-app-bar>-->
+  <!--    <v-main>-->
+  <div>
+      <NuxtPage />
+  </div>
+  <!--    </v-main>-->
+  <!--  </v-app>-->
+</template>
+
